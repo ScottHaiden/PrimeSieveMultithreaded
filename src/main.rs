b4fbd7251c2b38048c8cf9b14a3f9f649d8bacd3
Scott Haiden <scott.b.haiden@gmail.com>
2023-10-21 21:28:47 -0700

    Add a multithreaded prime finder

---
+++ src/main.rs
@@ -0,0 +1,85 @@
+use std::io::BufWriter;
+use std::io::Write;
+use std::sync::Arc;
+use std::sync::mpsc;
+
+use threadpool::ThreadPool;
+
+fn is_prime(i:u32, primes: &Vec<u32>) -> bool {
+    let limit = (i as f64).sqrt() as u32;
+    for &prime in primes {
+        if prime > limit { break; }
+        if i % prime == 0 { return false; }
+    }
+    return true;
+}
+
+fn find_prime_vec(max_prime: u32) -> Vec<u32> {
+    let mut ret = Vec::new();
+
+    for i in (3..max_prime).step_by(2) {
+        if !is_prime(i, &ret) { continue; }
+        ret.push(i)
+    }
+
+    return ret;
+}
+
+fn check_block(sender: mpsc::Sender<u32>, primes: Arc<Vec<u32>>, cands: Box<dyn Iterator<Item = u32>>) {
+    for cand in cands {
+        if !is_prime(cand, &primes) { continue; }
+        sender.send(cand).expect("Sending failed");
+    }
+}
+
+fn main() {
+    let (sender, receiver) = mpsc::channel::<u32>();
+
+    // Find out the highest prime we need to find in a single thread, in order to not miss any in
+    // the multi threaded scenario.
+    let max = u32::MAX;
+    let max_track = (max as f64).sqrt() as u32;
+
+    let primes = Arc::new(find_prime_vec(max_track));
+
+    let printer = std::thread::spawn(move || {
+        let mut all_primes = receiver.iter().collect::<Vec<u32>>();
+        all_primes.sort();
+
+        let mut out = BufWriter::new(std::io::stdout());
+        for prime in all_primes {
+            let bytes = prime.to_be_bytes();
+            out.write_all(&bytes).expect("write failed");
+        }
+    });
+
+    let threadpool = ThreadPool::new(std::thread::available_parallelism().unwrap().get());
+    for i in 1.. {
+        // Point returns a multiple of the square root of the maximum number we're checking to, if
+        // it's less than max, otherwise max itself.
+        let point = |i: u32| {
+            return match max_track.checked_mul(i) {
+                Some(n) => n,
+                None => u32::MAX,
+            };
+        };
+        // Set 'low' to the next odd number greater than the current multiple of the square root of
+        // the maximum number we're checking to.
+        let low = match point(i).checked_add(1) {
+            Some(n) => n | 1,
+            None => point(i),
+        };
+        let hi = point(i + 1);
+        let interval = Box::new((low..hi).step_by(2));
+
+        let primes = primes.clone();
+        let sender = sender.clone();
+        threadpool.execute(move || { check_block(sender, primes, interval); });
+        if hi == max { break; }
+    }
+    for &prime in primes.iter() { sender.send(prime).expect("send failed"); }
+    drop(sender);
+
+    threadpool.join();
+    printer.join().expect("Failed to join printer");
+}
